8c08703031f4ebc044e353135a57bb308e4c65f6
Matahari Ramadhan <ramadhan.matahari24@gmail.com>
2021-09-23 21:58:37 +0200

    Add get method for API

--- index.js
+++ index.js
@@ -14,6 +14,9 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
+app.get("/", (req, res) => {
+  res.send("Hello to Memories API");
+});
 app.use("/posts", postRoutes);
 
 const PORT = process.env.PORT || 5000;
